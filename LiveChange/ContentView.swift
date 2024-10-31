37027170241b10c957aef1e8aa9aa84a301f631d
andyyou <andyyu0920@gmail.com>
2024-10-31 11:08:49 +0800

    Initial Commit

---
+++ LiveChange/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveChange
+//
+//  Created by YOUZONGYAN on 2024/10/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiveChange/LiveChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveChangeApp.swift
+//  LiveChange
+//
+//  Created by YOUZONGYAN on 2024/10/31.
+//
+
+import SwiftUI
+
+@main
+struct LiveChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
